55172d9a64509d11e2248b80c5d746586a0c651b
DarmonNoah <noah.darmon@ecole-isitech.fr>
2024-02-01 09:23:31 +0100

    Update package.json & index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //import {toto} from "./module.js";
 
 //console.log(toto);
+
+import Express from "express";
+
+const app = Express();
+
+app. listen(3000, () => {
+console. log("Server running on port 3000");
+});
